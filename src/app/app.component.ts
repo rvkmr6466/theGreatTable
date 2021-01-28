c3e15af7f121219bf51dd532edb7bb852b7fdd19
Ravi Kumar <Ravi Kumar>
2021-01-29 00:01:13 +0530

    initial design for the Great table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  title = 'theGreatTable';
+	  selector: 'app-root',
+	  templateUrl: './app.component.html',
+	  styleUrls: ['./app.component.scss']
+	})
+export class AppComponent implements OnInit {
+	  title = 'theGreatTable';
+	  public toogleTrue: boolean = false;
+	  public day:number;
+	  public totalAmount: number = 350.5550;
+	  public totalDrinks: number = 25.50;
+
+  	public weekDays : {} = {
+	    0: "Monday",
+	    1: "Tuesday",
+	    2: "Wednesday",
+	    3: "Thrusday",
+	    4: "Friday",
+	    5: "Saturday",
+	    6: "Sunday"
+	};
+
+	public buttonItems: Array<{}> = [
+	{
+		0: "Drink"
+	},
+	{
+		1: "Food"
+	},
+	{
+		2: "Dessert"	
+	}
+];
+
+	ngOnInit(){
+		var d = new Date();
+		this.day = d.getDay()
+	}
+	  
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { ContentPanelComponent } from './content-panel/content-panel.component';
+import { SidePanelComponent } from './side-panel/side-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentPanelComponent,
+    SidePanelComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
